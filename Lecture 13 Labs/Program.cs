40279a942ed4919d5c13109a2885dcd19466befe
Upendra Samaranayake <humanfrailtz@gmail.com>
2020-11-29 17:30:17 -0800

    Add project files.

---
+++ Lecture 13 Labs/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Lecture_13_Labs
+{
+    class rectangle
+    {
+        static void Main(string[] args)
+        {
+            rectangle rect = new rectangle();
+            mydelegate rectdale = new mydelegate(rect.area);
+            rectdale += rectdale + new mydelegate(rect.perimeter);
+            rectdale.Invoke(6.0, 4.5);
+            Console.WriteLine();
+            rectdale.Invoke(15.3, 11.4);
+        }
+
+        //declaring delgate
+        public delegate void mydelegate(double height, double width);
+        public void area(double height, double width)
+        {
+            Console.WriteLine("Area is: " + (width * height));
+        }
+        public void perimeter(double height, double width)
+        {
+            Console.WriteLine("Perimeter is: " + (2*(width * height)));
+        }
+    }
+}
